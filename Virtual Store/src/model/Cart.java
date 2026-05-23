a4a713ec10ce9a137d8c15bb97f502e7c12c9602
alexrmr08 <walter.romero123.2@gmail.com>
2026-05-23 11:00:55 -0500

    feat: implementar atributos y métodos base de Cart y Product.

--- Virtual Store/src/model/Cart.java
+++ Virtual Store/src/model/Cart.java
@@ -2,6 +2,38 @@ package model;
 
 public class Cart {
     
-    dawdasdads
+    private List<Product> products;
+    private DiscountStrategy discountStrategy;
+
+    public Cart() {
+        this.products = new ArrayList<>();
+        this.discountStrategy = new NoDiscount();
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+        System.out.println("--> Producto agregado: " + product);
+    }
+
+    public double calculateSubtotal() {
+        double subtotal = 0;
+        for (Product product : products) {
+            subtotal += product.getPrice();
+        }
+        return subtotal;
+    }
+
+    public double calculateTotal() {
+        double subtotal = calculateSubtotal();
+        return discountStrategy.applyDiscount(subtotal);
+    }
+
+    public void setDiscountStrategy(DiscountStrategy discountStrategy) {
+        this.discountStrategy = discountStrategy;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
 
 }
